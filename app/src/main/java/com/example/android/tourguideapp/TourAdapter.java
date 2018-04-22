351756627cc96cb05e9ea529dc74e7b004ebe5bb
liwona <liwona@users.noreply.github.com>
2018-04-22 22:53:04 +0200

    ViewPager and TOurAdapter added

---
+++ app/src/main/java/com/example/android/tourguideapp/TourAdapter.java
@@ -0,0 +1,35 @@
+package com.example.android.tourguideapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Iwona on 22.04.2018.
+ */
+
+public class TourAdapter extends FragmentPagerAdapter {
+
+    public TourAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new CultureFragment();
+        } else if (position == 1){
+            return new CultureFragment();
+        } else if (position == 2){
+            return new CultureFragment();
+        } else {
+            return new CultureFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
+
