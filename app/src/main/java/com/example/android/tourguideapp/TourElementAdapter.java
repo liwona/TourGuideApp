c52f60281a98a7c3111b8c7a75861aa33b928288
liwona <liwona@users.noreply.github.com>
2018-04-23 19:28:25 +0200

    LastChanges

--- app/src/main/java/com/example/android/tourguideapp/TourElementAdapter.java
+++ app/src/main/java/com/example/android/tourguideapp/TourElementAdapter.java
@@ -9,14 +9,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
-
-/**
- * Created by Iwona on 22.04.2018.
- */
 
 public class TourElementAdapter extends ArrayAdapter<TourElement> {
 
@@ -101,5 +96,4 @@ public class TourElementAdapter extends ArrayAdapter<TourElement> {
         // so that it can be shown in the ListView
         return listItemView;
     }
-
 }
