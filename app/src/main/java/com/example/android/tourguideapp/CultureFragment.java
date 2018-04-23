5a4d95560eca1815873b7d71e5ff8107226ab511
liwona <liwona@users.noreply.github.com>
2018-04-23 16:23:29 +0200

    CultureFragment done & visual polish for App

--- app/src/main/java/com/example/android/tourguideapp/CultureFragment.java
+++ app/src/main/java/com/example/android/tourguideapp/CultureFragment.java
@@ -17,10 +17,26 @@ public class CultureFragment extends Fragment {
         final ArrayList<TourElement> tourElements = new ArrayList<TourElement>();
         tourElements.add(new TourElement("Wawel Castle",
                 "Wawel 5, 31-001 Kraków", "A castle residency build " +
-                "for polish monarchy"));
+                "for polish monarchy", R.drawable.wawel));
         tourElements.add(new TourElement("Main Square",
                 "30-062 Kraków", "It is the principal urban space " +
                 "located at the center of the city"));
+        tourElements.add(new TourElement("Kraków Cloth Hall (Sukiennice)",
+                "Rynek Główny 1-3, 30-001 Kraków", " It is the " +
+                "central feature of the main market square in the Kraków Old Town"));
+        tourElements.add(new TourElement("Oskar Schindler's Enamel Factory",
+                "Lipowa 4, 30-702 Kraków", "A former metal item factory in " +
+                "Kraków, is now host to two museums: the Museum of Contemporary Art in Kraków, " +
+                "on the former workshops, and a branch of the Historical Museum of the City of " +
+                "Kraków"));
+        tourElements.add(new TourElement("St. Mary's Basilica (Kościół Mariacki)",
+                "plac Mariacki 5, 31-042 Kraków", "A Brick Gothic " +
+                "church adjacent to the Main Market Square in Kraków, Poland. Built in the " +
+                "14th century", R.drawable.mariacki));
+        tourElements.add(new TourElement("Kazimierz",
+                "Kazimierz, Kraków", "It is a historical " +
+                "district of Kraków and Kraków Old Town, Poland."));
+
 
         // Create an {@link TourElementAdapter}, whose data source is a list of {@link TourElement}. The
         // adapter knows how to create list items for each item in the list.
